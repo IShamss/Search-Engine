ae1e9bece21d9c5df1f618f9c7ed52770e060a0b
KarimMutwally <63321772+AbdallahShedid1@users.noreply.github.com>
2022-05-19 02:22:51 +0200

    Create UserInterface.java

---
+++ src/Interface/UserInterface.java
@@ -0,0 +1,28 @@
+package Interface;
+import java.io.*;
+import javax.servlet.http.*;
+public class UserInterface extends HttpServlet {
+  /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    public UserInterface(){
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException {
+        String searchQuery = request.getParameter("searchQuery");
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("05-19-00-31-56-78.html"));
+            String str;
+            while ((str = in.readLine()) != null) {
+                contentBuilder.append(str);
+            }
+            in.close();
+        } catch (IOException e) {
+        }
+        String content = contentBuilder.toString();
+        response.setContentType("text/html");
+        response.getWriter().println(content);
+    }  
+}
